01fc85cb7fba72feca5dbffba6dc9a8ca7b020ea
FlorianChrx <florianchiro@hotmail.fr>
2019-05-26 18:35:20 +0200

    Add Rotation elements

---
+++ src/rotation/RotationController.java
@@ -0,0 +1,16 @@
+package rotation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class RotationController {
+	@FXML
+	private Label label;
+	@FXML
+	private Slider slider;
+	@FXML
+	private TextField textField;
+	
+}
---
+++ src/rotation/RotationWindow.java
@@ -0,0 +1,7 @@
+package rotation;
+
+import javafx.stage.Stage;
+
+public class RotationWindow extends Stage {
+
+}
